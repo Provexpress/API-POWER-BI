13843f937fecd757362edcdb2029cf1dc76d35dd
Felipe Cárdenas <cardenasdaniel178@gmail.com>
2026-05-19 11:40:11 -0500

    Set Power Query SharePoint parameters

--- power_query/00_BaseURL_Parametro.m
+++ power_query/00_BaseURL_Parametro.m
@@ -1,8 +1,9 @@
 // PARAMETRO: BaseURL
 // Define la URL completa de la carpeta donde estan publicados los CSV.
+// La consulta/parametro en Power BI debe llamarse exactamente: BaseURL
 // Debe terminar en "/".
 //
-// Ejemplo:
-//   "https://provexpress-my.sharepoint.com/personal/especialista_preventa_provexpress_com_co/Documents/PowerBI/Proveexpress/"
+// Usar este valor:
+//   https://provexpress-my.sharepoint.com/personal/especialista_preventa_provexpress_com_co/Documents/PowerBI/Proveexpress/
 
-"https://TU-CARPETA-CSV/" meta [IsParameterQuery=true, Type="Text", IsParameterQueryRequired=true]
+"https://provexpress-my.sharepoint.com/personal/especialista_preventa_provexpress_com_co/Documents/PowerBI/Proveexpress/" meta [IsParameterQuery=true, Type="Text", IsParameterQueryRequired=true]
--- power_query/00_SharePointSite_Parametro.m
+++ power_query/00_SharePointSite_Parametro.m
@@ -1,8 +1,9 @@
 // PARAMETRO: SharePointSite
 // Define el sitio SharePoint/OneDrive que se autentica en Power BI Service.
-// No debe incluir la carpeta Documents/PowerBI/Proveexpress.
+// La consulta/parametro en Power BI debe llamarse exactamente: SharePointSite
+// No debe incluir la carpeta /Documents/PowerBI/Proveexpress/.
 //
-// Ejemplo:
-//   "https://provexpress-my.sharepoint.com/personal/especialista_preventa_provexpress_com_co"
+// Usar este valor:
+//   https://provexpress-my.sharepoint.com/personal/especialista_preventa_provexpress_com_co
 
-"https://TU-SITIO-SHAREPOINT" meta [IsParameterQuery=true, Type="Text", IsParameterQueryRequired=true]
+"https://provexpress-my.sharepoint.com/personal/especialista_preventa_provexpress_com_co" meta [IsParameterQuery=true, Type="Text", IsParameterQueryRequired=true]
